7d4c99ec95114c4dd2728b77da8adde1c750a3f1
marzglav <feg.leg.crypto@gmail.com>
2022-11-24 20:06:31 +0100

    JS 1 predavanje

---
+++ numbers.js
@@ -0,0 +1,61 @@
+/* 
+var a = 1;
+var b;
+
+// Deskriptivna varijabla - lower camel case
+var myNumber = 10;
+var pi = 3.14;
+
+// Deklaracija varijable
+var deklaracija;
+
+console.log(deklaracija);
+console.log(typeof deklaracija);
+
+// Assignment
+deklaracija = -100.1;
+
+console.log(deklaracija);
+console.log(typeof deklaracija);
+
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(10 * 12);
+console.log(10 / 3);
+console.log(10 % 3);
+
+//bigint se koristi za velike brojke
+
+var result = 1 + (5 * 10) / 4;
+
+console.log(result);
+
+console.log(Math.pow(10, 3));
+console.log(Math.sqrt(1000, 3));
+console.log(Math.round(13.2));
+console.log(Math.round(13.50000001));
+console.log(Math.ceil(13.000001));
+console.log(Math.floor(13.9999999));
+
+var myString = 13;
+
+console.log(String(myString));
+console.log(typeof typeof myString);
+ */
+debugger;
+
+var i = 1;
+
+console.log(++i);
+console.log(i++);
+console.log(i);
+console.log(--i);
+console.log(i);
+console.log(i--);
+console.log(i);
+
+i = i + 1;
+//Kraci zapis gornjeg primjera
+i += 1;
+
+console.log(i);
---
+++ string.js
@@ -0,0 +1,14 @@
+var myFirstString = "Hello world!";
+
+console.log(myFirstString);
+console.log(typeof myFirstString);
+
+var myNumber = "1";
+
+console.log(myNumber);
+console.log(typeof myNumber);
+
+// Casting
+console.log(typeof Number(myNumber));
+// Coercion
+console.log(typeof +myNumber);
