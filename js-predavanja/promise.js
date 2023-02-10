70873235f88032198d3b209adb03c045b09ce6f2
marzglav <feg.leg.crypto@gmail.com>
2023-02-10 10:26:59 +0100

    Async await

--- js-predavanja/promise.js
+++ js-predavanja/promise.js
@@ -2,23 +2,27 @@
 // Fetch vraca promise koji wrappa Response objekt
 // Promise ima metodu then koja unwrappa promise
 
-async function request() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const data = await res.json();
+function promiseRequest() {
+  const promise = fetch("https://jsonplaceholder.typicode.com/todos/1");
 
-  console.log(data);
+  // Pending -> Fulfilled  - poziva se then metoda
+  // Pending -> Rejected  -  poziva se catch metoda
+
+  console.log({ promise });
+
+  promise
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 }
-
-request();
-
 /* 
-const promise = fetch("https://jsonplaceholder.typicode.com/todos/1");
-const responsePromise = promise.then((res) => res.json());
+// Catch sprijecava sa se program ugasi ukoliko promise throwa error
+setInterval(() => console.log("Tick tock..."), 1000);
 
-responsePromise.then((res) => {
-  console.log(res);
-});
- */
+// Sinkrona linija izvrsava se odma
+console.log("END PROGRAM"); 
+*/
+
 /* 
 console.log("Sync", promise);
 
@@ -26,3 +30,27 @@ setTimeout(() => {
   console.log("Timeout", promise);
 }, 1000);
  */
+
+// Async await nacin pisanja koda
+// VAZNO! Funkcija tipa async sve sto returna wrappa u promise
+
+async function request() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await res.json();
+
+    console.log(data);
+
+    console.log(
+      `User ${data.userId} ${
+        data.completed ? "has" : "hasnt"
+      } completed his task`
+    );
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+request();
