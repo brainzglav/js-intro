914ca8405a8007f648e6df40087a6a456679d234
marzglav <feg.leg.crypto@gmail.com>
2023-02-22 20:08:35 +0100

    Klase

--- js-predavanja/class.js
+++ js-predavanja/class.js
@@ -1,25 +1,41 @@
-class Student {
-  #id;
+class Human {
   name;
   surname;
-  percentage = 50;
+  weight;
+  height;
+  sex;
+
+  constructor({ name, surname, weight, height, sex }) {
+    this.name = name;
+    this.surname = surname;
+    this.weight = weight;
+    this.height = height;
+    this.sex = sex;
+  }
+}
+
+class Student extends Human {
+  #id;
+  percentage;
+  passedPercentage = 50;
 
   get fullName() {
     return `${this.name} ${this.surname}`;
   }
 
   get hasPassed() {
-    return this.percentage > 50;
+    return this.percentage > this.passedPercentage;
   }
 
   set id(id) {
     this.#id = id;
   }
 
-  constructor({ name, surname = "Horvat", percentage }) {
-    this.name = name;
-    this.surname = surname;
+  constructor({ percentage, passedPercentage, ...rest }) {
+    super(rest);
+
     this.percentage = percentage;
+    this.passedPercentage = passedPercentage;
   }
 
   printStudentPercentage() {
@@ -43,15 +59,31 @@ function StudentFactory(name, surname, percentage = 50) {
 // Konstruktorski poziv student klase
 const { users } = require("../db.json");
 
-console.log(users);
+console.log(human);
 
-const student = new Student({});
+//console.log(users);
 
+const student = new Student({
+  name: "Ante",
+  surname: "Antic",
+  sex: "Male",
+  percentage: 45,
+  passedPercentage: 50,
+});
+/* 
+console.log("1", student.hasPassed, student);
+
+student.passedPercentage = 40;
+
+console.log("2", student.hasPassed, student);
+ */
+/* 
 const studentList = users.map((student) => new Student(student));
 
 studentList.forEach((student) =>
   console.log(student.fullName, student.hasPassed)
-);
+); 
+*/
 //const studentFromFactory = StudentFactory("Antea", "Antic");
 
 //studentFromFactory.fun();
